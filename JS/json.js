593a2e7f2ebc39c79908c7d342c20598389ddfab
Md Saiyedul Morsalin <mdsaiyedulmorsalin.code@gmail.com>
2024-05-25 23:50:20 +0600

    added web document

--- JS/json.js
+++ JS/json.js
@@ -1,3 +1,13 @@
 // JavaScript Object Notation -->JSON
 // JSON.stringify()
-// JSON placeholder
+// JSON placeholder,GET data, Display data on UI...
+console.log("fatching");
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then(response => response.json())
+.then(json => console.log(json))
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+.then(res =>res.json())  //.json is not similar but close to JSON.parse
+.then(data =>{
+    console.log(data)
+})

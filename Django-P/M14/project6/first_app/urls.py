b3b95b4c1524cb93c1590de23132f60799a7bbd6
Md Saiyedul Morsalin <mdsaiyedulmorsalin.code@gmail.com>
2024-06-09 01:01:50 +0600

    added delete data from frontend

--- Django-P/M14/project6/first_app/urls.py
+++ Django-P/M14/project6/first_app/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index_page,name='index_page'),
+    path('home/',views.home,name='home'),
+    path('delete/<int:roll>',views.delete_student,name='delete_student'),
 ]
--- Django-P/M14/project6/first_app/views.py
+++ Django-P/M14/project6/first_app/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . import models
 # Create your views here.
 def index_page (request):
     return render(request,'./first_app/index.html')
+
+def home(request):
+    student = models.Student.objects.all()
+    # print(student)
+    return render(request,'./first_app/home.html',{'data':student})
+
+def delete_student(request,roll):
+    std = models.Student.objects.get(pk = roll).delete()
+    student = models.Student.objects.all()
+    print(std) 
+    return redirect('home')

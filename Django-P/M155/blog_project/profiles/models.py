10ccb685636d8f2bfdcb6c810a896b1de849051d
Md Saiyedul Morsalin <mdsaiyedulmorsalin.code@gmail.com>
2024-06-15 22:20:03 +0600

    all backend function emplement done

--- Django-P/M155/blog_project/profiles/models.py
+++ Django-P/M155/blog_project/profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from author.models import Author
 # Create your models here.
+class Profiles(models.Model):
+    name = models.CharField(max_length=100,default=None)
+    about = models.TextField()
+    author = models.OneToOneField(Author,on_delete=models.CASCADE)
+    # print(Author.name)
+    # print("Profiles")
+    def __str__(self):
+        return self.name
+        # return self.author

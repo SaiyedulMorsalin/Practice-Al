81ca8b89c016fb10692e7c3c9a30a9d1052454ac
Md Saiyedul Morsalin <mdsaiyedulmorsalin.code@gmail.com>
2024-06-21 00:49:32 +0600

    add new M17

--- Django-P/M17 new/project8/first_app/urls.py
+++ Django-P/M17 new/project8/first_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('login/',views.login,name='login_page'),
+    path('login/',views.userlogin,name='login_page'),
     path('signup/',views.signup,name='signup_page'),
     path('profiles/',views.profile,name='profile_page'),
+    path('logout/',views.userlogout,name='logout_page'),
+    path('pass_change/',views.pass_change,name='pass_change'),
 ]
--- Django-P/M17 new/project8/first_app/views.py
+++ Django-P/M17 new/project8/first_app/views.py
@@ -1,13 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import SignUpForm
 # Create your views here.
-
-def login(request):
-    return render(request,'login.html')
+from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm,SetPasswordForm
+from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
+def userlogin(request):
+    if not request.user.is_authenticated:
+        if request.method =='POST':
+            form = AuthenticationForm(request=request,data = request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['username']
+                userpass = form.cleaned_data['password']
+                user = authenticate(username = name,password = userpass)
+                if user is not None:
+                    login(request,user)
+                    return redirect('profile_page')
+                
+        
+        else:
+            form = AuthenticationForm()
+        return render(request,'login.html',{'form':form})
+    else:
+        return redirect('profile_page')
 
 def signup(request):
-    form = SignUpForm()
-    return render(request,'sign_up.html',{'form':form})
+    if not request.user.is_authenticated:
+        if request.method =='POST':
+            form = SignUpForm(request.POST)
+            if form.is_valid():
+                form.save()
+                print(form.cleaned_data)
+                
+        else:
+            form = SignUpForm()
+        return render(request,'sign_up.html',{'form':form})
+    else:
+        return redirect('login_page')
 
 def profile(request):
-    return render(request,'profile.html')
+    if request.user.is_authenticated:
+        return render(request,'profile.html',{'user':request.user})
+    else:
+        return redirect('login_page')
+
+def userlogout(request):
+    logout(request)
+    return redirect('login_page')
+
+
+def pass_change(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = PasswordChangeForm(user=request.user,data=request.POST)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request,request.user)
+                return redirect('profile_page')
+            
+        else:
+            form = PasswordChangeForm(user=request.user)
+        return render(request,'passchange.html',{'form':form})
+    else:
+        return redirect('login_page')

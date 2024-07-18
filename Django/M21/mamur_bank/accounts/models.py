1c35426be33f6c7f2883311cfdbcccad6b7fdab4
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-07-18 08:55:14 +0600

    add bank_part1

---
+++ Django/M21/mamur_bank/accounts/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from .constants import ACCOUNT_TYPE,GENDER_TYPE
+# Create your models here.
+class UserBankAccount(models.Model):
+    user = models.OneToOneField(User,related_name='account',on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=10,choices=ACCOUNT_TYPE)
+    account_no = models.PositiveIntegerField(unique=True)
+    birth_date = models.DateField()
+    gender = models.CharField(max_length=10,choices=GENDER_TYPE)
+    initial_deposit_date = models.DateField(auto_now_add=True)
+    balance = models.DecimalField(default=0,max_digits=12,decimal_places=2)
+    def __str__(self):
+        return str(self.account_no)
+    
+    
+class UserAddress(models.Model):
+    user = models.OneToOneField(User,related_name='address',on_delete=models.CASCADE)
+    street_address =models.CharField(max_length=150)
+    city = models.CharField(max_length=100)
+    postal_code = models.PositiveIntegerField(max_length=4)
+    country = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return str(self.user.email)

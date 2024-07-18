aae88039cab2744d4a0f922acc5a7691abff01b1
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-07-18 14:07:03 +0600

    added

--- Django/M21/mamur_bank/accounts/views.py
+++ Django/M21/mamur_bank/accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth import login 
 from django.views.generic import FormView
+from django.contrib.auth.views import LoginView,LogoutView
 from .forms import UserRegistrationForm
+from django.contrib.auth import login,logout
 # Create your views here.
 
 class UserRegistrationView(FormView):
@@ -14,4 +16,17 @@ class UserRegistrationView(FormView):
         user = form.save()
         login(self.request,user)
         return super().form_valid(form)
+    
+
+class UserLogin(LoginView):
+    template_name = 'user_login.html'
+    def get_success_url(self):
+        return reverse_lazy('home_page')
+    
+class UserLogout(LogoutView):
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        
+        return reverse_lazy('home_page')
     

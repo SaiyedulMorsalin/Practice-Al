9c837eb504e2277160110c4224bbdfa729f9a2a3
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-07-26 19:01:39 +0600

    added transactions

--- Django/M21/mamur_bank/accounts/views.py
+++ Django/M21/mamur_bank/accounts/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.urls import reverse_lazy
 from django.contrib.auth import login 
-from django.views.generic import FormView
+from django.views.generic import FormView,View
 from django.contrib.auth.views import LoginView,LogoutView
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm,UserUpdateForm
 from django.contrib.auth import login,logout
 # Create your views here.
 
@@ -27,6 +27,18 @@ class UserLogout(LogoutView):
     def get_success_url(self):
         if self.request.user.is_authenticated:
             logout(self.request)
-        
         return reverse_lazy('home_page')
     
+
+class UserBankAccountUpdateView(View):
+    template_name = 'profile.html'
+    
+    def get(self,request):
+        form = UserUpdateForm(instance = request.user)
+        return render(request,self.template_name,{'form':form})
+    def post(self,request):
+        form = UserUpdateForm(request.POST,instance = request.user)
+        if form.is_valid():
+            # form.save()
+            return redirect('profile')
+        return render(request,self.template_name,{'form':form})

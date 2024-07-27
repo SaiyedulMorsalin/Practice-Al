9ea526c09edac02a55384959dd92c1c9ae8ebb33
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-07-27 18:40:27 +0600

    added transaction views

--- Django/M21/mamur_bank/transactions/forms.py
+++ Django/M21/mamur_bank/transactions/forms.py
@@ -23,13 +23,30 @@ class DepositForm(TransactionForm):
         min_deposit_amount = 100
         amount = self.cleaned_data.get('amount') # user er filap kra form theke amra amount field er value niye aslam...
         if amount<min_deposit_amount:
-            raise forms.ValidationError(f'You need to deposit at least {min_deposit_amount}')
+            raise forms.ValidationError(f'You need to deposit at least {min_deposit_amount}$')
         return amount
 
 
 class WithdrawalForm(TransactionForm):
-    pass
+    def clean_amount(self):
+        account = self.account
+        min_withdraw_balance = 50
+        max_withdraw_balance = 25000
+        balance = account.balance
+        amount = self.cleaned_data.get('amount')
+        if amount<min_withdraw_balance:
+            raise forms.ValidationError(f'You can withdraw at least {min_withdraw_balance}$')
+        if amount > max_withdraw_balance:
+            raise forms.ValidationError(f'You can withdraw maximum {max_withdraw_balance}$')
+        if amount>balance:
+            raise forms.ValidationError(f'You have {balance}$ in you account'
+                                        'You can not withdraw more than your account balance')
+        
+        return amount
+        
 
-class LoanForm(TransactionForm):
-    pass
+class LoanRequestForm(TransactionForm):
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+        return amount
                     
